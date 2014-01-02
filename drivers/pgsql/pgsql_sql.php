6dc9ed0d525d715598020fb03478e1777f8a972f
Timothy J. Warren <tim@timshomepage.net>
2014-01-02 12:36:50 -0500

    Update copyright for the new year

--- drivers/pgsql/pgsql_sql.php
+++ drivers/pgsql/pgsql_sql.php
@@ -5,7 +5,7 @@
  * Free Query Builder / Database Abstraction Layer
  *
  * @author 		Timothy J. Warren
- * @copyright	Copyright (c) 2012 - 2013
+ * @copyright	Copyright (c) 2012 - 2014
  * @link 		https://github.com/aviat4ion/Query
  * @license 	http://philsturgeon.co.uk/code/dbad-license
  */
