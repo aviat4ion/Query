484548dc49de9d3f7612795735b51ad84227b3d6
Timothy J. Warren <tim@timshomepage.net>
2014-02-19 13:45:05 -0500

    Remove PHP 5.2 from testing

--- tests/core/core.php
+++ tests/core/core.php
@@ -7,20 +7,20 @@
  * @author 		Timothy J. Warren
  * @copyright	Copyright (c) 2012 - 2014
  * @link 		https://github.com/aviat4ion/OpenSQLManager
- * @license 	http://philsturgeon.co.uk/code/dbad-license 
+ * @license 	http://philsturgeon.co.uk/code/dbad-license
  */
 
 // --------------------------------------------------------------------------
 /**
  * CoreTest class - Compatibility and core functionality tests
- * 
+ *
  * @extends UnitTestCase
  */
 class CoreTest extends Query_TestCase {
-	
+
 	/**
 	 * __construct function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -28,25 +28,25 @@ class CoreTest extends Query_TestCase {
 	{
 		parent::__construct();
 	}
-	
+
 	// --------------------------------------------------------------------------
-	
+
 	/**
 	 * TestPHPVersion function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
 	public function testPHPVersion()
 	{
-		$this->assertTrue(version_compare(PHP_VERSION, "5.2", "ge"));
+		$this->assertTrue(version_compare(PHP_VERSION, "5.3", "ge"));
 	}
-	
+
 	// --------------------------------------------------------------------------
-	
+
 	/**
 	 * TestHasPDO function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -54,8 +54,8 @@ class CoreTest extends Query_TestCase {
 	{
 		// PDO class exists
 		$this->assertTrue(class_exists('PDO'));
-		
-		
+
+
 		// Make sure at least one of the supported drivers is enabled
 		$supported = array(
 			'mysql',
@@ -63,11 +63,11 @@ class CoreTest extends Query_TestCase {
 			'odbc',
 			'sqlite',
 		);
-		
+
 		$drivers = PDO::getAvailableDrivers();
-		
+
 		$num_supported = count(array_intersect($drivers, $supported));
-		
+
 		$this->assertTrue($num_supported > 0);
 	}
 }
