c60e768f4e9ecadaa50a45c9ab5ab2cbe69fdb30
Timothy J Warren <tim@timshomepage.net>
2016-07-15 15:50:28 -0400

    First attempt to set up gitlab ci

---
+++ build/docker_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+
+# Install phpunit, the tool that we will use for testing
+curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+# Install mysql driver
+# Here you can install any other extension that you need
+docker-php-ext-install pdo_mysql
+docker-php-ext-install pdo_sqlite
+docker-php-ext-install pdo_postgre
+#docker-php-ext-install pdo_oci
+docker-php-ext-install interbase
