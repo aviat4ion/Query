0d13d8e078f8ba03086bd561f099d09686750b75
Timothy J Warren <tim@timshomepage.net>
2016-07-18 11:12:03 -0400

    Another attempt at gitlab ci

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -6,10 +6,8 @@
 set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
-# And for some reason apt-utils is not installed
 apt-get update -yqq
-apt-get install apt-utils
-apt-get install git sqlite -yqq
+apt-get install git libpq-dev libmysqlclient18 -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -18,7 +16,6 @@ chmod +x /usr/local/bin/phpunit
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install pdo_mysql
-docker-php-ext-install pdo_sqlite
 docker-php-ext-install pdo_postgre
 #docker-php-ext-install pdo_oci
 docker-php-ext-install interbase
