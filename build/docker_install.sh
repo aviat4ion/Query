a2d5092b5ea9c31fa09660737c041cd069c41f3c
Timothy J Warren <tim@timshomepage.net>
2018-01-26 09:00:45 -0500

    Attempt to fix Gitlab build for PHP 7.2

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -10,7 +10,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git libpq-dev libmysqlclient18 unzip -yqq
+apt-get install git libpq-dev unzip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
